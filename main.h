87cb27f38889a07313f47ca1823a43f6b1c7748d
Arran Schlosberg <me@arranschlosberg.com>
2013-06-15 18:48:18 +1000

    README + inclusion of license heading in all files

--- main.h
+++ main.h
@@ -3,6 +3,28 @@
  *
  *  Created on: 23/03/2013
  *      Author: arran
+ 
+---------------------------------------------------------------------------------------
+
+Copyright 2013 Arran Schlosberg.
+
+This file is part of https://github.com/aschlosberg/SNP (SNP)
+
+    SNP is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SNP is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SNP. If not, see <http://www.gnu.org/licenses/>.
+
+---------------------------------------------------------------------------------------
+ 
  */
 
 #ifndef MAIN_H_
