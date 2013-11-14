6755fdb106390eca9f5a5e9dfeefa355fe54936a
Arran Schlosberg <me@arranschlosberg.com>
2013-11-14 19:32:20 +1100

    Added FASTA to single line file conversion in server-side of web interface

--- web/run.php
+++ web/run.php
@@ -1,9 +1,16 @@
 <?php
-$files = array("msa", "del", "neut", "classify");
+$files = array("msa", "del", "neut", "novel");
 $pref = "CompressGV-".time()."-".substr(md5(uniqid()), 0, 8);
+
+$_POST['msa'] = str_replace('&gt;', '>', $_POST['msa']);
+
 foreach($files as $k => $f){
 	$files[$k] = "/tmp/{$pref}_{$f}";
 	file_put_contents($files[$k], trim($_POST[$f]));
+}
+
+if(strpos($_POST['msa'], ">")!==false){
+	`./fastaToSingleLine.sh $files[0] > {$files[0]}_single; mv {$files[0]}_single $files[0];`;
 }
 
 $exec = "./grantham ".implode(" ", $files);
